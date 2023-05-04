6464a3c64d6b8f51db5d169e49fb1f12e3fef818
Bamba Ba <bba@wheretech.it>
2023-05-04 19:35:53 +0200

    Port

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/home.html');
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log('Server is listening on port 3000...');
 });
